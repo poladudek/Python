a649b4b7272eb91d5306e8d35043999897383057
Pola <146646425+poladudek@users.noreply.github.com>
2024-11-02 19:26:32 +0100

    added assert

--- Z4/zad_4.7.py
+++ Z4/zad_4.7.py
@@ -2,11 +2,15 @@ def flatten(sequence):
     flattened_list = []
     for element in sequence:
         if isinstance(element, (list, tuple)):
-            flattened_list.extend(flatten(element)) #extend bo operujemy na listach/krotkach
+            flattened_list.extend(flatten(element))
         else:
             flattened_list.append(element)
     return flattened_list
 
 seq = [1, (2, 3), [], [4, (5, 6, 7)], 8, [9]]
+expected_result = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 result = flatten(seq)
+
+assert result == expected_result, f"Wynik oczekiwany:\n{expected_result}\nWynik otrzymany:\n{result}"
+
 print(result)
