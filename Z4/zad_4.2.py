b00e637098e10466f6daab751cf4db7b5048aab0
Pola <146646425+poladudek@users.noreply.github.com>
2024-11-02 19:21:19 +0100

    added assert

--- Z4/zad_4.2.py
+++ Z4/zad_4.2.py
@@ -19,8 +19,15 @@ def make_grid(rows, cols):
         full_rec += "+---" * cols + "+\n"
     return full_rec
 
+
+expected_ruler_1 = "|....|....|....|....|....|....|....|....|....|....|....|....|....|....|....|....|\n0    1    2    3    4    5    6    7    8    9    10   11   12   13   14   15   "
+expected_grid_1 = "+---+---+---+---+---+---+---+\n|   |   |   |   |   |   |   |\n+---+---+---+---+---+---+---+\n|   |   |   |   |   |   |   |\n+---+---+---+---+---+---+---+\n|   |   |   |   |   |   |   |\n+---+---+---+---+---+---+---+\n|   |   |   |   |   |   |   |\n+---+---+---+---+---+---+---+\n|   |   |   |   |   |   |   |\n+---+---+---+---+---+---+---+\n"
+
 ruler_1 = make_ruler(15)
 grid_1 = make_grid(5, 7)
+
+assert ruler_1 == expected_ruler_1, f"Wynik oczekiwany:\n{expected_ruler_1}\nWynik otrzymany:\n{ruler_1}"
+assert grid_1 == expected_grid_1, f"Wynik oczekiwany:\n{expected_grid_1}\nWynik otrzymany:\n{grid_1}"
 
 print(ruler_1)
 print(grid_1)
