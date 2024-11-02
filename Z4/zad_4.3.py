3b776a0487aa74d9ebbb52be5114275c8de5e497
Pola <146646425+poladudek@users.noreply.github.com>
2024-11-02 19:23:33 +0100

    assert added

--- Z4/zad_4.3.py
+++ Z4/zad_4.3.py
@@ -1,10 +1,16 @@
 def factorial(n):
     result = 1
-    if (n == 0): return 1
-    if (n == 1): return 1
-    for i in range (1, n+1):
+    if n == 0: 
+        return 1
+    if n == 1: 
+        return 1
+    for i in range(1, n + 1):
         result *= i
     return result
 
+expected_result = 6
 result = factorial(3)
+
+assert result == expected_result, f"Wynik oczekiwany:\n{expected_result}\nWynik otrzymany:\n{result}"
+
 print(result)
