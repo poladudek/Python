3e73dadfd8ff985c1434ca042c65f51eb88b63eb
Pola <146646425+poladudek@users.noreply.github.com>
2024-11-02 19:25:50 +0100

    added assert

--- Z4/zad_4.6.py
+++ Z4/zad_4.6.py
@@ -6,5 +6,9 @@ def sum_seq(sequence):
     return sequence[0] + sum_seq(sequence[1:])
 
 sequence = [(1, 2), 3, 4, 5, [6, 7, 8]] 
+expected_result = 36
 result = sum_seq(sequence)
+
+assert result == expected_result, f"Wynik oczekiwany:\n{expected_result}\nWynik otrzymany:\n{result}"
+
 print(result)
